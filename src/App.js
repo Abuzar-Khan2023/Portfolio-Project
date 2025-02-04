e34d70a362678c2eb42423bb8f476b7c3ec22afa
Abuzar Khan <149166915+abuzarkhanse@users.noreply.github.com>
2025-02-04 22:47:37 +0000

    Remove Gal and Back updates

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Route path="/project" element={<Project />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/gallery" element={<Gallery />} />
+        {/* <Route path="/gallery" element={<Gallery />} /> */}
         <Route path="/downloadcv" element={<DownloadCv />} />
         {/* <Route path="*" element={<NotFound />} /> Fallback route */}
       </Routes>
